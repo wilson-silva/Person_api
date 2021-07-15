b187e6e3fdaa4257ae289e135dac90f3db02902c
Wilson <wilson-gs@hotmail.com>
2021-07-15 18:12:58 -0300

    Desenvolvido método all list

--- src/main/java/one/innovationone/personapi/service/PersonService.java
+++ src/main/java/one/innovationone/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import one.innovationone.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class PersonService {
@@ -19,8 +22,6 @@ public class PersonService {
 
 
     @Autowired
-//    public PersonService() {
-//    }
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
@@ -34,5 +35,13 @@ public class PersonService {
                 .builder()
                 .message("created person with ID" + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
